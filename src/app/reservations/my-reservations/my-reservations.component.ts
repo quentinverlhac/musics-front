2d5d72e30c9df7032c5b74214150f7ff2756f837
Rémi Calixte <remi.calixte@student.ecp.fr>
2017-12-12 16:29:50 +0100

    fix

--- src/app/reservations/my-reservations/my-reservations.component.ts
+++ src/app/reservations/my-reservations/my-reservations.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ReservationService } from "../reservation.service";
 
 @Component({
-  selector: 'app-reservations',
-  templateUrl: './reservations.component.html',
-  styleUrls: ['./reservations.component.css']
+  selector: 'app-my-reservations',
+  templateUrl: './my-reservations.component.html',
+  styleUrls: ['./my-reservations.component.css']
 })
 export class MyReservationsComponent implements OnInit {
 
