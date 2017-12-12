4f08849b62b8b1f69dc345e7adb5bfa58cb6378c
Rémi Calixte <remi.calixte@student.ecp.fr>
2017-12-12 11:04:16 +0100

    add menu items

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
+  @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
 
   constructor() { }
 
-  ngOnInit() {
+  onHover() {
+    this.trigger.openMenu();
   }
 
 }
